4c6d4ea32a614b660d73d6d4de0ecfc2c43bacf2
Eugene Skepner <github@skepner.eu>
2017-01-31 09:48:07 +0100

    drawing chart development

--- py/acmacs_chart/draw.py
+++ py/acmacs_chart/draw.py
@@ -3,6 +3,8 @@
 # license.
 # ----------------------------------------------------------------------
 
+import copy
+import logging; module_logger = logging.getLogger(__name__)
 from .hidb_access import get_hidb
 from .vaccines import vaccines
 from acmacs_chart_backend import ChartDraw, PointStyle
@@ -22,15 +24,30 @@ def draw_chart(output_file, chart, settings, hidb_dir, output_width):
 
 # ----------------------------------------------------------------------
 
+sStyleByVaccineType = {
+    "previous": {"size": 20, "fill": "blue", "outline": "black"},
+    "current": {"size": 20, "fill": "red", "outline": "black"},
+    "surrogate": {"size": 20, "fill": "pink", "outline": "black"},
+    }
+
+sStyleByPassageType = {
+    "egg": {"aspect": 0.75},
+    "reassortant": {"aspect": 0.75, "rotation": 0.5},
+    "cell": {}
+    }
+
 def mark_vaccines(chart_draw, chart, hidb_dir):
     hidb = get_hidb(chart=chart, hidb_dir=hidb_dir)
     for vaccine_entry in vaccines(chart=chart):
+        # module_logger.debug('{}'.format(vaccine_entry))
         antigens = chart.vaccines(vaccine_entry["name"], hidb)
-        for vaccine_data, name in [[antigens.egg(), "EGG:"], [antigens.cell(), "CELL:"], [antigens.reassortant(), "REASSORTANT:"]]:
+        for passage_type in ["egg", "reassortant", "cell"]:
+            vaccine_data = getattr(antigens, passage_type)()
             if vaccine_data:
-                print(name, vaccine_data.antigen_index, vaccine_data.antigen.full_name())
-                # for es in vaccine_data.homologous_sera:
-                #     print("  ", es.serum.full_name(), " tables:", es.number_of_tables(), " most-recent:", es.most_recent_table, " species:", es.serum.serum_species(), sep="")
+                style = copy.deepcopy(sStyleByVaccineType[vaccine_entry["type"]])
+                style.update(sStyleByPassageType[passage_type])
+                module_logger.info('Marking vaccine {} {}'.format(vaccine_data.antigen_index, vaccine_data.antigen.full_name()))
+                chart_draw.modify_point_by_index(vaccine_data.antigen_index, make_point_style(style))
 
 # ----------------------------------------------------------------------
 
