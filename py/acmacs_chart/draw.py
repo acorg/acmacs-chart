028b5d220f7c8f5f8382a62a04492ce32034adaa
Eugene Skepner <github@skepner.eu>
2017-01-31 09:02:16 +0100

    drawing chart development

--- py/acmacs_chart/draw.py
+++ py/acmacs_chart/draw.py
@@ -13,9 +13,20 @@ def draw_chart(output_file, chart, settings, hidb_dir, output_width):
     chart_draw.prepare()
     #chart_draw.scale_points(3)
     chart_draw.all_grey()
-    chart_draw.modify_point_by_index(0, PointStyle().fill("blue").outline("black").size(20))
+    # chart_draw.modify_point_by_index(0, PointStyle().fill("blue").outline("black").size(20))
+    # chart_draw.modify_point_by_index(0, make_point_style({"fill": "blue", "outline": "black", "size": 20}))
     # chart_draw.modify_point_by_index(10, acmacs_chart.PointStyle(fill="red", outline="black", size=20))
     chart_draw.draw(str(output_file), output_width)
+
+# ----------------------------------------------------------------------
+
+def make_point_style(data):
+    ps = PointStyle()
+    for k, v in data.items():
+        setter = getattr(ps, k, None)
+        if setter:
+            setter(v)
+    return ps
 
 # ----------------------------------------------------------------------
 ### Local Variables:
