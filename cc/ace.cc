6f365c8b29b6fb508997dac5f850b47719c28f45
Eugene Skepner <github@skepner.eu>
2017-02-08 10:53:40 +0100

    ace writer improvement

--- cc/ace.cc
+++ cc/ace.cc
@@ -295,6 +295,36 @@ Chart* import_chart(std::string buffer)
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
 
+template <typename RW> inline jsw::writer<RW>& operator <<(jsw::writer<RW>& writer, const Antigen& aAntigen)
+{
+    return writer << jsw::start_object
+                  << jsw::key("N") << aAntigen.name()
+                  << jsw::if_not_empty("D", aAntigen.date())
+                  << jsw::if_not_empty("L", aAntigen.lineage())
+                  << jsw::if_not_empty("P", aAntigen.passage())
+                  << jsw::if_not_empty("R", aAntigen.reassortant())
+                  << jsw::if_not_empty("l", aAntigen.lab_id())
+                  << jsw::if_not_empty("S", aAntigen.semantic())
+                  << jsw::if_not_empty("a", aAntigen.annotations())
+                  << jsw::if_not_empty("c", aAntigen.clades())
+                  << jsw::end_object;
+}
+
+template <typename RW> inline jsw::writer<RW>& operator <<(jsw::writer<RW>& writer, const Serum& aSerum)
+{
+    return writer << jsw::start_object
+                  << jsw::key("N") << aSerum.name()
+                  << jsw::if_not_empty("I", aSerum.serum_id())
+                  << jsw::if_not_empty("L", aSerum.lineage())
+                  << jsw::if_not_empty("P", aSerum.passage())
+                  << jsw::if_not_empty("R", aSerum.reassortant())
+                  << jsw::if_not_empty("S", aSerum.semantic())
+                  << jsw::if_not_empty("a", aSerum.annotations())
+                  << jsw::if_non_negative("h", aSerum.homologous())
+                  << jsw::if_not_empty("s", aSerum.serum_species())
+                  << jsw::end_object;
+}
+
 template <typename RW> inline jsw::writer<RW>& operator <<(jsw::writer<RW>& writer, const ChartInfo& aChartInfo)
 {
     return writer << jsw::start_object
