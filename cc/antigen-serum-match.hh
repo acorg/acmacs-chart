9d67dfb222d43c276e50852156d66d692f439bc7
Eugene Skepner <github@skepner.eu>
2017-09-21 17:57:07 +0200

    AntigenSerumMatchScore improvements

--- cc/antigen-serum-match.hh
+++ cc/antigen-serum-match.hh
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <functional>
+#include <regex>
 
 #include "acmacs-base/string-matcher.hh"
 
@@ -37,6 +38,24 @@ class AntigenSerumMatch
 #endif
 
 }; // class AntigenSerumMatch
+
+// ----------------------------------------------------------------------
+
+namespace _antigen_serum_match
+{
+#pragma GCC diagnostic push
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wexit-time-destructors"
+#pragma GCC diagnostic ignored "-Wglobal-constructors"
+#endif
+
+    inline static const std::regex sReCell{" (MDCK|SIAT|QMC)[0-9]"};
+    inline static const std::regex sReEgg{" (.*/)?E[0-9]\\b"};
+    inline static const std::regex sReReassortant{" (NYMC|NIB|[IB]VR|RESVIR|SB|(UW)?RG|VI|CBER|REASSORTANT)(?![A-Z])"};
+
+#pragma GCC diagnostic pop
+
+} // namespace _antigen_serum_match
 
 // ----------------------------------------------------------------------
 
@@ -84,6 +103,7 @@ template <typename Data> class AntigenSerumMatchScore
     const Data* mAntigen;
     string_match::score_t mName, mFull;
 
+
     inline void preprocess(std::string name, string_match::score_t aNameScoreThreshold)
         {
             const auto antigen_name = mAntigen->name();
@@ -91,11 +111,17 @@ template <typename Data> class AntigenSerumMatchScore
             if (aNameScoreThreshold == 0)
                 aNameScoreThreshold = static_cast<string_match::score_t>(name.length() * name.length() * 0.05);
             if (mName >= aNameScoreThreshold) {
+                using namespace _antigen_serum_match;
                 const auto full_name = mAntigen->full_name();
+                const auto name_part_size = static_cast<int>(antigen_name.size());
                 mFull = std::max({
-                    for_subst(full_name, antigen_name.size(), name, " CELL", {" MDCK", " SIAT", " QMC"}, {}),
-                    for_subst(full_name, antigen_name.size(), name, " EGG", {" E"}, {"NYMC", "IVR", "NIB", "RESVIR", "RG", "VI", "REASSORTANT"}),
-                    for_subst(full_name, antigen_name.size(), name, " REASSORTANT", {" NYMC", " IVR", " NIB", " RESVIR", " RG", " VI", " REASSORTANT"}, {})
+                        for_subst(full_name, name_part_size, name, " CELL", sReCell, nullptr),
+                        for_subst(full_name, name_part_size, name, " EGG", sReEgg, &sReReassortant),
+                        for_subst(full_name, name_part_size, name, " REASSORTANT", sReReassortant, nullptr)
+
+                    // for_subst(full_name, antigen_name.size(), name, " CELL", {" MDCK", " SIAT", " QMC"}, {}),
+                    // for_subst(full_name, antigen_name.size(), name, " EGG", {" E"}, {"NYMC", "IVR", "NIB", "RESVIR", "RG", "VI", "REASSORTANT"}),
+                    // for_subst(full_name, antigen_name.size(), name, " REASSORTANT", {" NYMC", " IVR", " NIB", " RESVIR", " RG", " VI", " REASSORTANT"}, {})
                     });
                 if (mFull == 0)
                     mFull = string_match::match(full_name, name);
@@ -103,27 +129,48 @@ template <typename Data> class AntigenSerumMatchScore
             }
         }
 
-    inline string_match::score_t for_subst(std::string full_name, size_t name_part_size, std::string name, std::string keyword, std::initializer_list<const char*>&& subst_list, std::initializer_list<const char*>&& negative_list)
-    {
-        string_match::score_t score = 0;
-        const auto pos = name.find(keyword);
-        if (pos != std::string::npos) { // keyword is in the lookup name
-            if (!std::any_of(negative_list.begin(), negative_list.end(), [&full_name,name_part_size](const auto& e) -> bool { return full_name.find(e, name_part_size) != std::string::npos; })) {
-                for (const auto& subst: subst_list) {
-                    if (full_name.find(subst + 1, name_part_size) != std::string::npos) { // subst (without leading space) must be present in full_name in the passage part
+    // inline string_match::score_t for_subst(std::string full_name, size_t name_part_size, std::string name, std::string keyword, std::initializer_list<const char*>&& subst_list, std::initializer_list<const char*>&& negative_list)
+    //     {
+    //         string_match::score_t score = 0;
+    //         const auto pos = name.find(keyword);
+    //         if (pos != std::string::npos) { // keyword is in the lookup name
+    //             if (!std::any_of(negative_list.begin(), negative_list.end(), [&full_name,name_part_size](const auto& e) -> bool { return full_name.find(e, name_part_size) != std::string::npos; })) {
+    //                 for (const auto& subst: subst_list) {
+    //                     if (full_name.find(subst + 1, name_part_size) != std::string::npos) { // subst (without leading space) must be present in full_name in the passage part
+    //                         std::string substituted(name, 0, pos);
+    //                         substituted.append(subst);
+    //                         score = std::max(score, string_match::match(full_name, substituted));
+    //                     }
+    //                     else if (score == 0)
+    //                         score = keyword_in_lookup; // to avoid using name-with-keyword-not-replaced for matching
+    //                 }
+    //             }
+    //             else                // string from negative_list present in full_name, ignore this name
+    //                 score = keyword_in_lookup;
+    //         }
+    //         return score;
+    //     }
+
+    inline string_match::score_t for_subst(std::string full_name, int name_part_size, std::string name, std::string keyword, const std::regex& positive, const std::regex* negative)
+        {
+            string_match::score_t score = 0;
+            const auto pos = name.find(keyword);
+            if (pos != std::string::npos) { // keyword is in the lookup name
+                if (!negative || !std::regex_search(full_name.begin() + name_part_size, full_name.end(), *negative)) {
+                    std::smatch match;
+                    if (std::regex_search(full_name.cbegin() + name_part_size, full_name.cend(), match, positive)) {
                         std::string substituted(name, 0, pos);
-                        substituted.append(subst);
+                        substituted.append(match.str());
                         score = std::max(score, string_match::match(full_name, substituted));
                     }
                     else if (score == 0)
                         score = keyword_in_lookup; // to avoid using name-with-keyword-not-replaced for matching
                 }
+                else                // string from negative regex present in full_name, ignore this name
+                    score = keyword_in_lookup;
             }
-            else                // string from negative_list present in full_name, ignore this name
-                score = keyword_in_lookup;
+            return score;
         }
-        return score;
-    }
 };
 
 // ----------------------------------------------------------------------
